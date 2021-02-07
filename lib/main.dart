13a87437069af453d4692a1e52d377238372bdaf
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-02-07 13:22:10 +0530

    Remove flutter_highlight dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'dart:js' as js;
 import 'package:flutter/material.dart';
 import 'package:scaffold_responsive/scaffold_responsive.dart';
 import 'package:flutter_highlight/flutter_highlight.dart';
+import 'package:flutter_highlight/themes/atom-one-dark-reasonable.dart';
 
 import './widgets/code_block.dart';
 import './widgets/editable_code_block.dart';
-import './themes/atom-one-dark-reasonable.dart';
 
 void main() {
   runApp(MyApp());
