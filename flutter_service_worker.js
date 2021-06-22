238a229840c117fcc204b3366fc85dfbc05fc8c2
tusharsadhwani <tusharsadhwani@users.noreply.github.com>
2021-06-22 12:28:10 +0000

    deploy: 064fe6460cf3053c1050afce3abfe6867a098812

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,18 +3,17 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/NOTICES": "cc0e8b9f0584413c8863eb16e71f91c4",
-"assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
-"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"index.html": "556f416cfefd4fb2d7296026cc504e15",
-"/": "556f416cfefd4fb2d7296026cc504e15",
-"main.dart.js": "3017074f8034ecd42d6ea09c439ecc80",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+  "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"manifest.json": "0cb42c35c9bc229ff908bdacad2dd056",
 "version.json": "c207eff58b2d6f156873b1346006adae",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796"
+"manifest.json": "0cb42c35c9bc229ff908bdacad2dd056",
+"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/NOTICES": "c24f4f93ea469332ba1a4782c2411493",
+"assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
+"main.dart.js": "f20ae2f6471ebcf2692bd23b72e097bb",
+"index.html": "d02f8539b369f5622eee1bfa85f0bf80",
+"/": "d02f8539b369f5622eee1bfa85f0bf80"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -32,7 +31,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
